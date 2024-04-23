9b74d117dc62dd9683c037f41596508e219f0dbc
the-other-mariana <mariana.avalos.arce@gmail.com>
2024-04-22 23:25:00 -0600

    fix: ignore cmake files
    Signed-off-by: the-other-mariana <mariana.avalos.arce@gmail.com>

--- template/node/CMakeFiles/node.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/node.dir/Node.cpp.o"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang CXX)
-  include(CMakeFiles/node.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
